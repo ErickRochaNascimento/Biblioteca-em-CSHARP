5b66c301cdc2806e540088beb368af415e5899e4
Erick Rocha Nascimento <erickrochaluciano@gmail.com>
2026-02-28 20:23:12 -0300

    adiciona método listar e ajusta lógica de adição de livros

--- ClasseBiblioteca.js
+++ ClasseBiblioteca.js
@@ -13,14 +13,21 @@ export class Biblioteca{
 
         if( !(livro.anoPublicacao > 2026) && livro.isbn.length == 13 ){
             console.log(`Livro ${livro.titulo} adicionado`);
+            this.livros.push(livro);
         }else{
             console.log(`Livro ${livro.titulo} não adicionado.`);
         }
 
     }
 
+    listar(){
+        console.log(`Listar Livros`);
+        this.livros.forEach(l => {
+            console.log(`Titulo: ${l.titulo} - Autor: ${l.autor} - ISBN: ${l.isbn} - Ano de Publicação: ${l.anoPublicacao} - Disponível: ${l.disponivel}`);
+        })
+    }
 
-
+    
 
 
 }
---
+++ index.js
@@ -0,0 +1,13 @@
+import { Livro } from "./ClasseLivro.js";
+import { Biblioteca } from "./ClasseBiblioteca.js";
+const BibliotecaPrincipal = new Biblioteca();
+
+const l1 = new Livro("Erick", "Erick", "1234567890123",2025,true);
+const l2 = new Livro("MAtheus", "Matheus", "1234567890",2025,true);
+const l3 = new Livro("Rebeca", "rebeca", "1234567890123",2027,true);
+
+BibliotecaPrincipal.adicionar(l1);
+BibliotecaPrincipal.adicionar(l2);
+BibliotecaPrincipal.adicionar(l3);
+
+BibliotecaPrincipal.listar();
