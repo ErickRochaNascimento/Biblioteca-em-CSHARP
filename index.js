7f05ebe152fba7609fdbe1a7bab9426b3abd1ed2
Erick Rocha Nascimento <erickrochaluciano@gmail.com>
2026-02-28 20:48:18 -0300

    adiciona livro vazio e ajusta chamadas de listar e remover

--- index.js
+++ index.js
@@ -5,12 +5,17 @@ const BibliotecaPrincipal = new Biblioteca();
 const l1 = new Livro("Erick", "Erick", "1234567890123",2025,true);
 const l2 = new Livro("MAtheus", "Matheus", "1234567890",2025,true);
 const l3 = new Livro("Rebeca", "rebeca", "1234567890123",2027,true);
+const l4 = new Livro("", "rebeca", "1234567890123",2025,true);
 
 BibliotecaPrincipal.adicionar(l1);
 BibliotecaPrincipal.adicionar(l2);
 BibliotecaPrincipal.adicionar(l3);
+BibliotecaPrincipal.adicionar(l4);
 
 BibliotecaPrincipal.listar();
 
 BibliotecaPrincipal.buscarPorISBN("1234567890123");
 BibliotecaPrincipal.buscarPorISBN("1234567890");
+
+BibliotecaPrincipal.removerLivro("1234567890123");
+BibliotecaPrincipal.listar();
