51c3be009a32425f8eb869f62495cc572b77435c
ErickRochaNascimento <erickrochaluciano@gmail.com>
2026-03-02 11:15:09 -0300

    corrige ISBN dos livros no arquivo Server.js

--- Server.js
+++ Server.js
@@ -5,9 +5,9 @@ import { Biblioteca } from "./ClasseBiblioteca.js";
 
 const BibliotecaPrincipal = new Biblioteca();
 
-const l1 = new Livro("Erick", "Erick", "1234567890123", 2025, true);
+const l1 = new Livro("Erick", "Erick", "1234567890121", 2025, true);
 const l2 = new Livro("MAtheus", "Matheus", "1234567890", 2025, true);
-const l3 = new Livro("Rebeca", "rebeca", "1234567890123", 2027, true);
+const l3 = new Livro("Rebeca", "rebeca", "1234567890122", 2027, true);
 const l4 = new Livro("Rebeca", "rebeca", "1234567890123", 2025, true);
 
 BibliotecaPrincipal.adicionarLivro(l1);
